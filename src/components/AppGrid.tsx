8acf0174ae9a7aa1fd08b7e5d5f3f62866e04f69
Moaz <99875565+moaazelsharkawy@users.noreply.github.com>
2025-09-20 03:52:09 +0300

    Update AppGrid.tsx

--- src/components/AppGrid.tsx
+++ src/components/AppGrid.tsx
@@ -14,7 +14,7 @@ export function AppGrid({ onExternalClick }: AppGridProps) {
     { type: 'deposit', key: 'deposit', href: 'https://sallanet.com/tokens-deposit/', useCustomIcon: true, image: '/lovable-uploads/5efbad70-8c28-42f4-999a-e33ba97dd136.png' },
     { type: 'withdraw', key: 'withdraw', href: 'https://sallanet.com/tokens-withdraw/', useCustomIcon: true, image: '/lovable-uploads/9a245025-bc13-4de6-9593-eab6ec20ac92.png' },
     { type: 'stQr', key: 'stQr', href: 'https://sallanet.com/qr-code/', useCustomIcon: true, image: '/lovable-uploads/st-qr-icon.png' },
-    { type: 'transfer', key: 'transfer', href: 'https://salla-shop.com/st-transfer/', useCustomIcon: true, image: '/lovable-uploads/8532e579-cb11-4528-89ba-5111bdb13006.png' },
+    { type: 'transfer', key: 'transfer', href: 'https://sallanet.com/transfer/', useCustomIcon: true, image: '/lovable-uploads/8532e579-cb11-4528-89ba-5111bdb13006.png' },
     { type: 'payRequest', key: 'payRequest', href: 'https://salla-shop.com/st-pay-request/', useCustomIcon: true, image: '/lovable-uploads/ddaaf2cb-9091-4ea0-86ed-115a98c4cf52.png' },
     { type: 'escrow', key: 'escrow', href: 'https://salla-shop.com/st-escrow/', useCustomIcon: true, image: '/lovable-uploads/5a533289-5d36-4788-bfcd-c3ab0b3eb88b.png' },
     { type: 'explorer', key: 'explorer', href: 'https://salla-shop.com/st-explorer/', useCustomIcon: true, image: '/lovable-uploads/832e9ce6-d367-4ff8-9148-212144eccca8.png' },
