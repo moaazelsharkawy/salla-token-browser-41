69e2f0cbb6c287859f08fb9992a781bc46dc9d18
Moaz <99875565+moaazelsharkawy@users.noreply.github.com>
2025-10-01 16:35:20 +0300

    Update AppGrid.tsx

--- src/components/AppGrid.tsx
+++ src/components/AppGrid.tsx
@@ -10,6 +10,7 @@ export function AppGrid({ onExternalClick }: AppGridProps) {
   const { t } = useTranslation();
 
   const apps = [
+    { type: 'ico', key: 'ico', href: 'https://sallanet.com/st-presale/', useCustomIcon: true, image: '/lovable-uploads/ico-icon.png' },
     { type: 'mining', key: 'mining', href: 'https://sallanet.com/st-system/', useCustomIcon: true, image: '/lovable-uploads/5c3e9ffe-8f37-4bf9-a759-f28ecc772177.png' },
     { type: 'deposit', key: 'deposit', href: 'https://sallanet.com/tokens-deposit/', useCustomIcon: true, image: '/lovable-uploads/5efbad70-8c28-42f4-999a-e33ba97dd136.png' },
     { type: 'withdraw', key: 'withdraw', href: 'https://sallanet.com/tokens-withdraw/', useCustomIcon: true, image: '/lovable-uploads/9a245025-bc13-4de6-9593-eab6ec20ac92.png' },
@@ -18,7 +19,6 @@ export function AppGrid({ onExternalClick }: AppGridProps) {
     { type: 'payRequest', key: 'payRequest', href: 'https://sallanet.com/pay-request/', useCustomIcon: true, image: '/lovable-uploads/ddaaf2cb-9091-4ea0-86ed-115a98c4cf52.png' },
     { type: 'escrow', key: 'escrow', href: 'https://sallanet.com/escrow/', useCustomIcon: true, image: '/lovable-uploads/5a533289-5d36-4788-bfcd-c3ab0b3eb88b.png' },
     { type: 'explorer', key: 'explorer', href: 'https://sallanet.com/st-explorer/', useCustomIcon: true, image: '/lovable-uploads/832e9ce6-d367-4ff8-9148-212144eccca8.png' },
-    { type: 'ico', key: 'ico', href: 'https://sallanet.com/st-presale/', useCustomIcon: true, image: '/lovable-uploads/ico-icon.png' },
     { type: 'virtualCards', key: 'virtualCards', href: 'https://sallanet.com/st-vpc/', useCustomIcon: true, image: '/lovable-uploads/28ed21c9-2da4-4633-9ed1-f2478e57902d.png' },
     { type: 'developerApi', key: 'developerApi', href: 'https://sallanet.com/merchant-wallet/', useCustomIcon: true, image: '/lovable-uploads/9fbe103d-21f9-4b2b-8cd4-beb6cc579a0f.png' },
     { type: 'commerce', key: 'commerce', href: 'https://sallanet.com/st-commerce/', useCustomIcon: true, image: '/lovable-uploads/d61dec61-2cf6-45c2-96cb-1f1a964c5bff.png' },
